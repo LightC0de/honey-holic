1ca773731590629ebb89a17ec036bbb6cf033a62
kengup <kegups@gmail.com>
2019-05-26 17:29:37 +0300

    added adaptive slider to main stock block

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('It works!');
+$('.stock-slider').slick({
+    dots: true,
+    dotsClass: "my-dots",
+    appendDots: $(".stock-slider-nav"),
+    infinite: true,
+    slidesToShow: 2,
+    arrows: false,
+    responsive: [{
+        breakpoint: 580,
+        settings: {
+            slidesToShow: 1,
+        }
+    }]
+});
